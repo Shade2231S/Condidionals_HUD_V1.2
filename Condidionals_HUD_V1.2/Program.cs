ac5e839af07696588f6555d4a19ae03492031823
Shade2231S <matthewmcculloch149@gmail.com>
2026-09-16 13:44:54 -0300

    Initial commit

---
+++ Condidionals_HUD_V1.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condidionals_HUD_V1._2
+{
+    internal class Program
+    {
+        static float Health = 100f;
+        enum weapon
+        {
+            Pistol,
+            Rifle,
+            Shotgun,
+            Sniper,
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
